8a2fcccbdd1040be0eda395dd835666d8a4696f3
George Sabourin <gcs516@student.bham.ac.uk>
2017-02-04 17:58:03 +0000

    Got objects received and sending by both the server and clients

--- src/game/networking/Client.java
+++ src/game/networking/Client.java
@@ -1,42 +1,56 @@
 package game.networking;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 /**
+ * Main client clas
  * Created by Daniel on 31/01/2017.
+ * Modified by George on 04/02/2017.
  */
 public class Client {
 
-    public Client(){
-    }
+	public static void main(String[] args){
+		if(args.length != 3){
+			System.out.println("Usage: java Client <username> <host> <port>");
+			System.exit(0);
+		}
 
-    public static void main(String[] args){
-        if(args.length != 2){
-            System.out.println("Usage: java Client <host> <port>");
-            System.exit(0);
-        }
-        String host = args[0];
-        int port = Integer.parseInt(args[1]);
+		String username = args[0];
+		String host = args[1];
+		int port = Integer.parseInt(args[2]);
 
-        try{
-            Socket outSocket = new Socket(host,port);
+		// Initialise
+		ObjectOutputStream objOut = null;
+		ObjectInputStream objIn = null;
+		Socket outSocket = null;
 
-            ObjectInputStream objIn = new ObjectInputStream(outSocket.getInputStream());
-            ObjectOutputStream objOut = new ObjectOutputStream(outSocket.getOutputStream());
+		try {
+			outSocket = new Socket(host,port);
+			System.out.println("DEBUG: Socket created");
+			objOut = new ObjectOutputStream(outSocket.getOutputStream());
+			objIn = new ObjectInputStream(outSocket.getInputStream());
+			System.out.println("DEBUG: I/O streams created");
+		} catch(UnknownHostException e) {
+			System.err.println("Error! Unknown host - " + host);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Error! Perhaps the server hasn't been started? " + e.getMessage());
+			System.exit(1);
+		}
 
-            while(true){
-                Object obj = objIn.readObject();
-                System.out.println(obj == null);
-            }
+		ClientSender client_sender = new ClientSender(username, objOut);
+		ClientReceiver client_receiver = new ClientReceiver(username, objIn);
 
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-
-        }
-
-    }
+		// Starting threads
+		client_sender.start();
+		client_receiver.start();
+		
+		// TODO: Closure method for threads
+	}
 }
