8ad5d2a0d36b133a7f761cb0ca57de132e8d9dbd
Jon Woodburn <jon.woodburn@wanadoo.fr>
2017-02-10 21:00:44 +0000

    Put the audio in a thread. Added atmospheric zombies sounds (badly)

--- src/game/Collision.java
+++ src/game/Collision.java
@@ -16,6 +16,8 @@ public class Collision {
     public static void checkBulletCollision(int bulletIndex, ArrayList<Bullet> bullets, ArrayList<Zombie> zombies, Player player) {
         for (int i = 0; i < zombies.size(); i++) {
             if (bullets.get(bulletIndex).getCollisionBox().intersects(zombies.get(i).getCollisionBox())) {
+            	Sound.zombieHit();
+                bullets.get(bulletIndex).damage(zombies.get(i), 25);
                 bullets.get(bulletIndex).damage(zombies.get(i), 25, player.conversionMode);
                 bullets.remove(bulletIndex);
                 break;
--- src/game/Sound.java
+++ src/game/Sound.java
@@ -1,25 +1,30 @@
 package game;
 
-import com.sun.glass.events.KeyEvent;
-
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Random;
 
 import javax.sound.sampled.*;
 
 public class Sound extends Thread{
     private String pistolSound = "src/game/sounds/pistol.wav";
     private String music = "src/game/sounds/music.wav";
-    private String zombieDeath = "src/game/sounds/zombie.wav";
+    private String zombieDeath = "src/game/sounds/zombieDeath.wav";
+    private String zombieSound ="src/game/sounds/zombieSound";
     private boolean running;
-    Clip musicClip;
+    Clip musicClip, gunClip;
     public InputHandler input;
+    private static boolean zombieHit;
+    Random rn, oneTwoOrThree;
 
     public Sound(InputHandler input) {
     	running = true;
     	this.input = input;
+    	zombieHit = false;
+    	rn = new Random();
+    	oneTwoOrThree = new Random();
     }
     
     public void run() {
@@ -32,7 +37,6 @@ public class Sound extends Thread{
     			
     			update();
     			
-    			
     			Thread.sleep(50);
     		}
     	} catch (InterruptedException e) {
@@ -41,8 +45,15 @@ public class Sound extends Thread{
     }
     
     public void update() {
+    	if(zombieHit) {
+    		zombieDeath();
+    		zombieHit = false;
+    	}
     	if(input.isMouseButtonDown(MouseEvent.BUTTON1)){
     		bulletSound();
+    	}
+    	if(this.rn.nextInt(75) == 25) {
+    		zombieSounds();
     	}
     	
     }
@@ -52,9 +63,17 @@ public class Sound extends Thread{
         try {
             File file = new File(fileName);
             if (file.exists()) {
+            	sound = AudioSystem.getClip();
                 AudioInputStream audioInp = AudioSystem.getAudioInputStream(file);
-                sound = AudioSystem.getClip();
                 sound.open(audioInp);
+                
+                sound.addLineListener(new LineListener() {
+                    public void update(LineEvent event) {
+                      if (event.getType() == LineEvent.Type.STOP) {
+                        event.getLine().close();
+                      }
+                    }
+                  });
             }
             else {
                 throw new RuntimeException("Sound: file not found: " + fileName);
@@ -70,7 +89,7 @@ public class Sound extends Thread{
             throw new RuntimeException("Sound: Input/Output Error: " + e);
         }
         catch (LineUnavailableException e) {
-            throw new RuntimeException("Sound: Line Unavailable: " + e);
+            throw new RuntimeException("Sound: Line Unavailable: " + e.getMessage());
         }
         
         return sound;
@@ -78,12 +97,33 @@ public class Sound extends Thread{
     	
 //play the bullet sound	
     public void bulletSound() {
-	    this.createClip(pistolSound).start();
+    	Clip gunClip = this.createClip(pistolSound);
+	    gunClip.start();
+	    System.out.println(gunClip.isOpen());
+	    
+    }
+    
+    public void turnDownVolume(Clip clip, Float fl) {
+    	FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+    	gainControl.setValue(fl);
+    }
+    
+    public static void zombieHit() {
+    	zombieHit = true;
     }
     
 //play the zombie death sound   
     public void zombieDeath() {
-    	this.createClip(zombieDeath).start();
+    	Clip zombie = this.createClip(zombieDeath);
+    	zombie.start();
+    }
+//play random zombie sounds    
+    public void zombieSounds() {
+    	int fileNum = oneTwoOrThree.nextInt(3) + 1;
+    	System.out.println(fileNum);
+    	String zombieS = zombieSound + fileNum + ".wav";
+    	Clip zombies = this.createClip(zombieS);
+    	zombies.start();
     }
     
 //play the music   
