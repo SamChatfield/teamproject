3f56b55addbe2e83bc196fcb7c373d9fd066c5ef
Daniel Tonks <dltonks@gmail.com>
2017-02-16 13:05:43 +0000

    Removed networking package. Tidied up code.

--- src/game/client/ClientReceiver.java
+++ src/game/client/ClientReceiver.java
@@ -1,42 +1,38 @@
 package game.client;
 
 import game.networking.SampleObject;
+import game.server.ServerGameState;
 
 import java.io.ObjectInputStream;
 
 /**
- * @author georgesabourin
+ * @author georgesabourin, Daniel Tonks
  * Class for receiving objects from the server and then determining what to do with them
  */
 public class ClientReceiver extends Thread {
 
 	private String username;
 	private ObjectInputStream objIn;
+	private ClientGameStateInterface inter;
 
 	/**
 	 * Constructor
 	 * @param username Name of user
 	 * @param objIn ObjectInputStream
 	 */
-	ClientReceiver(String username, ObjectInputStream objIn) {
+	ClientReceiver(String username, ObjectInputStream objIn, ClientGameStateInterface inter) {
 		this.username = username;
 		this.objIn = objIn;
+		this.inter = inter;
 	}
 
 	// Main method to run when thread starts
 	public void run() {
-		
 		System.out.println("DEBUG: ClientReceiver started");
 		try {
 			while(true) {
-//				System.out.println("Receiving object");
-				SampleObject obj1 = (SampleObject)objIn.readObject();
-				if(obj1 != null) {
-					System.out.println("Object = " +obj1.toString() + " | value = " + obj1.getValue());
-				}
-				else {
-					System.out.println("ERROR! Object is null!");
-				}	
+				ServerGameState state = (ServerGameState)objIn.readObject();
+				inter.update(state); // update the clients view of the game state.
 			}
 		} catch(Exception e) {
 			System.err.println("Error! " + e.getMessage());
